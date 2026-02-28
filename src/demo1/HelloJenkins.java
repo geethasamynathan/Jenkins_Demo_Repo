0da3b9c3150ec6cb0a124ec6d12d9ef62100f6ff
geethasamynathan <geethasamynathan2011@gmail.com>
2026-02-28 11:56:59 +0530

    Rename HelloJenkins class to MAIN_CLASS

--- src/demo1/HelloJenkins.java
+++ src/demo1/HelloJenkins.java
@@ -1,6 +1,6 @@
 package demo1;
 
-public class HelloJenkins {
+public class  MAIN_CLASS {
 	 public static void main(String[] args) {
 	        System.out.println("✅ Hello from Jenkins CI/CD Demo!");
 	        System.out.println("Build time: " + java.time.LocalDateTime.now());
